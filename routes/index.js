5a1ddcc1ae5cebb1716e1c102cdab4b6c383879a
surajsk <surajsk@SURAJSK-LAP.in.oracle.com>
2018-11-15 16:26:04 +0530

    UI Added for UrlShortener

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,73 @@ var router = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var shortid = require('shortid');
 var validurl = require('valid-url');
+var bodyParser = require('body-parser');
 
 var config = require('./config');
 
 var dbname = 'urlshort';
 
+router.use(bodyParser.urlencoded({ extended: true }));
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { placeHolderName: 'Enter Your Url', placeHolderEmail : "surajandraja@gmail.com"});
 });
+
+router.post('/generateShortId', function(req, res, next){
+	//console.log(req.body.originalUrl);
+
+	mongodb.connect('mongodb://' + config.db.host + '/' + config.db.name , function(error, client){
+	if(error){
+		throw error;
+	}
+	var db = client.db(dbname);
+	console.log("Connected to the Database urlshort in POST method");
+
+	var coll = db.collection('links');
+	//var params = req.params.url;
+	var params = req.body.originalUrl;
+	var host = req.get('host') + '/';
+
+	var dbFunction = function(db, callbackfunction){
+	//find whether that data is already present inside my database. If its there , then it will give that JSON, otherwise document will give null
+	coll.findOne({url : params},function(error, document){
+		if(document !== null){
+			//it means , data is already inside database. so ,it will give that doument as a JSON.
+			//so, no need to push it again. Just print it on the screen.
+			res.json({original_url : document.url , short_url : document.short });
+		}
+		else{
+			//Data is not there . So, push it in the database
+			var insertOnelink = {url : params , short : shortid.generate()};
+			//first, check whether Url is valid or not.
+			if(validurl.isUri(insertOnelink.url)){
+			//coll.insert(insertOnelink);
+			//console.log("ShortId : "+shortid.generate());
+			if(coll.insert(insertOnelink)){
+				//res.render('generateShortId', {oldUrl : params , newUrl : shortid.generate() });
+				coll.findOne({url : params}, function(error, document){
+					res.render('generateShortId', {oldUrl : document.url, newUrl : "http://localhost:3000/" + document.short})
+				});
+			}			
+			//res.json({original_url : host + params , short_url : host + shortid.generate()});
+			}
+			else{
+				console.log("Invalid Url");
+				res.render('error', {invalidError : 'Invalid Url : Make Sure that your Url is correct'})
+				//res.json({error : "Invalid Url : Make Sure that your Url is correct"});
+			}
+		}
+	});
+
+	};
+
+	dbFunction(db, function(){
+		db.close();
+	});
+	//res.send(params);
+});
+	});
 
 router.get('/new/:url(*)', function(req, res, next){
 	console.log(req.params.url);
@@ -37,7 +95,7 @@ router.get('/new/:url(*)', function(req, res, next){
 		if(document !== null){
 			//it means , data is already inside database. so ,it will give that doument as a JSON.
 			//so, no need to push it again. Just print it on the screen.
-			res.json({original_url : document.params , short_url : document.short });
+			res.json({original_url : document.url , short_url : document.short });
 		}
 		else{
 			//Data is not there . So, push it in the database
